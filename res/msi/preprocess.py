b2e7f1c5217d8b2dca48613f4a929d9432f5092a
szadang <70855632@qq.com>
2026-04-12 18:01:21 +0800

    优化项目路径

--- res/msi/preprocess.py
+++ res/msi/preprocess.py
@@ -27,15 +27,15 @@ g_arpsystemcomponent = {
     },
     "Contact": {
         "msi": "ARPCONTACT",
-        "v": "https://github.com/rustdesk/rustdesk",
+        "v": "https://github.com/szadang/rustdesk",
     },
     "HelpLink": {
         "msi": "ARPHELPLINK",
-        "v": "https://github.com/rustdesk/rustdesk/issues/",
+        "v": "https://github.com/szadang/rustdesk/issues/",
     },
     "ReadMe": {
         "msi": "ARPREADME",
-        "v": "https://github.com/rustdesk/rustdesk",
+        "v": "https://github.com/szadang/rustdesk",
     },
 }
 
